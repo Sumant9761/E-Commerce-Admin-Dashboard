16633e4592f75e60a44376dc0b6f4af64e622344
Sumant9761 <sumantkumaryadav7037@gmail.com>
2026-08-18 15:11:00 +0530

    Make fully responsive

--- src/Components/DashboardBoxes/index.jsx
+++ src/Components/DashboardBoxes/index.jsx
@@ -19,6 +19,12 @@ const DashboardBoxes = (props) => {
         spaceBetween={10}
         navigation={true}
         modules={[Navigation]}
+        breakpoints={{
+          320: { slidesPerView: 1, spaceBetween: 10 },
+          550: { slidesPerView: 2, spaceBetween: 10 },
+          768: { slidesPerView: 3, spaceBetween: 10 },
+          1024: { slidesPerView: 4, spaceBetween: 10 }
+        }}
         className="dashboardBoxesSlider"
       >
         <SwiperSlide>
